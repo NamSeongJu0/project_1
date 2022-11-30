a24abe1b2fb96f616ce6ee1c367f563c8b0a4bab
NamSeongJu0 <skaehdwn0@naver.com>
2022-11-30 18:54:16 +0900

    11/30
    왜 다시 안되냐고........................

--- Login.js
+++ Login.js
@@ -12,22 +12,24 @@ function chk() {
     var passwordbox1Len = passwordbox1Str.length;
 
 
+    // 아이디 및 패스워드 입력 받기
     if (idbox1Str == '') {
         alert('아이디를 입력하시오');
     }
-    else if (idbox1Len <= 8 || idbox1Len >= 15) {
-        alert('아이디를 8자리 이상, 15자리 이하로 입력하시오');
+    else if (idbox1Len < 8 || idbox1Len >= 15) {
+        alert('아이디를 9자리 이상, 15자리 이하로 입력하시오');
     } else {
         if (passwordbox1Str == '') {
             alert('패스워드를 입력하시오');
-            return false;
         }
         else if (passwordbox1Len <= 10 || passwordbox1Len >= 20) {
             alert('패스워드를 10자리 이상, 20자리 이하로 입력하시오');
+            return false;
         }
     }
 }
 
+// 로그인 및 여러 버튼들
 function log() {
     alert('로그인 쉬~일패!')
 }
