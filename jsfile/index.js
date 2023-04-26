878fb8d58c33c452d0b3d14ceb2a665889cc56ee
NamSeongJu0 <skaehdwn0@naver.com>
2023-04-26 11:06:14 +0900

    asdawd
    zcfzcs

--- jsfile/index.js
+++ jsfile/index.js
@@ -1,9 +1,9 @@
 // 디저트 호버 + 이벤트 깜빡임 효과
         $(document).ready(function () {
             $('#dessert img').hover(function () {
-                $(this).siblings('.best').fadeIn();
+                $(this).siblings('.best').stop().fadeIn();
             }, function () {
-                $(this).siblings('.best').fadeOut();
+                $(this).siblings('.best').stop().fadeOut();
             });
         });
 
