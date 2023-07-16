b787838b61031c5a24f42a71c05954f6248c2805
NamSeongJu0 <119376125+NamSeongJu0@users.noreply.github.com>
2023-07-16 19:35:16 +0900

    페이지
    07/16 수정

--- jsfile/Menu.js
+++ jsfile/Menu.js
@@ -1,20 +1,20 @@
         // 라벨 클릭 시 보여지는 스크립트
         $(function() {
-            $('.menuList label:nth-of-type(1)').click(function() {
+            $('.menuList label:nth-of-type(1), .co').click(function() {
                 $("#CoffeeList").fadeIn();
                 $("#IceBlendedList").hide();
                 $("#BakeryList").hide();
             });
 
 
-            $('.menuList label:nth-of-type(2)').click(function() {
+            $('.menuList label:nth-of-type(2), .ic').click(function() {
                 $("#IceBlendedList").fadeIn().css('display', 'flex');
                 $("#CoffeeList").hide();
                 $("#BakeryList").hide();
             });
 
 
-            $('.menuList label:nth-of-type(3)').click(function() {
+            $('.menuList label:nth-of-type(3), .ba').click(function() {
                 $("#BakeryList").fadeIn().css('display', 'flex');
                 $("#IceBlendedList").hide();
                 $("#CoffeeList").hide();
@@ -22,7 +22,7 @@
         });
 
 
-        // 커피 이미지 호버 시 상품 설명
+        // 메뉴 호버 시 상품 설명
         $(document).ready(function () {
             $('.textbox').hide();
 
@@ -31,24 +31,12 @@
             }, function () {
                 $(this).children('.textbox').stop().fadeOut();
             });
-        });
-
-
-        // 블렌디드 이미지 호버 시 상품 설명
-        $(document).ready(function () {
-            $('.textbox').hide();
 
             $('#IceBlendedList > div').hover(function () {
                 $(this).children('.textbox').stop().fadeIn().css('display', 'flex');
             }, function () {
                 $(this).children('.textbox').stop().fadeOut();
             });
-        });
-
-
-        // 베이커리 이미지 호버 시 상품 설명
-        $(document).ready(function () {
-            $('.textbox').hide();
 
             $('#BakeryList > div').hover(function () {
                 $(this).children('.textbox').stop().fadeIn().css('display', 'flex');
